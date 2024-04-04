e022157191dd5a340c1ca1d07fa73d2624b8ebcc
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2024-04-04 01:32:15 -0300

    final main for testing

--- part 1/main.c
+++ part 1/main.c
@@ -3,14 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// // lex.yy.h
-// int yylex();
-// extern char *yytext;
-// extern FILE *yyin;
-
-// int isRunning(void);
-// void initMe(void);
-
 int main(int argc, char **argv)
 {
   int tok;
@@ -19,30 +11,89 @@ int main(int argc, char **argv)
     printf(stderr, "Call: ./a.out file_name\n");
     exit(1);
   }
-  yyin = fopen(argv[1], "I");
+  yyin = fopen(argv[1], "r");
 
   hashInit();
   while (running)
   {
     tok = yylex();
+
     if (running == 0)
       break;
     switch (tok)
     {
+    case KW_CHAR:
+      printf("Found KW_CHAR\n");
+      break;
     case KW_INT:
-      printf("Encontrei KW_INT\n");
+      printf("Found KW_INT\n");
+      break;
+    case KW_FLOAT:
+      printf("Found KW_FLOAT\n");
+      break;
+    case KW_BOOL:
+      printf("Found KW_BOOL\n");
+      break;
+    case KW_IF:
+      printf("Found KW_IF\n");
+      break;
+    case KW_ELSE:
+      printf("Found KW_ELSE\n");
+      break;
+    case KW_WHILE:
+      printf("Found KW_WHILE\n");
+      break;
+    case KW_READ:
+      printf("Found KW_READ\n");
+      break;
+    case KW_PRINT:
+      printf("Found KW_PRINT\n");
+      break;
+    case KW_RETURN:
+      printf("Found KW_RETURN\n");
+      break;
+    case OPERATOR_LE:
+      printf("Found OPERATOR_LE\n");
+      break;
+    case OPERATOR_GE:
+      printf("Found OPERATOR_GE\n");
+      break;
+    case OPERATOR_EQ:
+      printf("Found OPERATOR_EQ\n");
+      break;
+    case OPERATOR_DIF:
+      printf("Found OPERATOR_DIF\n");
+      break;
+    case TK_IDENTIFIER:
+      printf("Found TK_IDENTIFIER\n");
       break;
     case LIT_INT:
-      printf("Encontrei LIT_INT\n");
+      printf("Found LIT_INT\n");
+      break;
+    case LIT_CHAR:
+      printf("Found LIT_CHAR\n");
+      break;
+    case LIT_REAL:
+      printf("Found LIT_REAL\n");
+      break;
+    // case LIT_FALSE:
+    //   printf("Found LIT_FALSE\n");
+    //   break;
+    // case LIT_TRUE:
+    //   printf("Found LIT_TRUE\n");
+    //   break;
+    case LIT_STRING:
+      printf("Found LIT_STRING\n");
       break;
     case TOKEN_ERROR:
-      printf("unexpected token %d\n", tok);
+      printf("Found token error\n");
       break;
     default:
-      printf("Encontrei %c\n", yytext[0]);
+      printf("Found %c\n", yytext[0]);
       break;
     }
   }
+  hashPrint();
   printf("Main done! File has %d lines\n", lineNumber);
   exit(0);
 }
