810691090a351a560f183f7ff2a6a1da399d2be2
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2024-08-07 01:10:08 -0300

    feat(): new main

--- part 3/main.c
+++ part 3/main.c
@@ -2,28 +2,57 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "hash.h"
+#include "ast.h"
+
+extern FILE *yyin;
+extern AST *getRoot();
+int yyparse();
+int getLineNumber();
 
 int main(int argc, char **argv)
 {
-  int tok;
-  if (argc < 2)
-  {
-    printf(stderr, "Call: ./a.out file_name\n");
-    exit(1);
-  }
-  yyin = fopen(argv[1], "r");
-  if (yyin == 0)
-  {
-    fprintf(stderr, "Cannot open file %s\n", argv[1]);
-    exit(2);
-  }
+    FILE *output;
 
-  hashInit();
+    if (argc < 3)
+    {
+        fprintf(stderr, "Call: ./etapa3 file_name file_name\n");
+        exit(1);
+    }
 
-  yyparse();
+    yyin = fopen(argv[1], "r");
+    if (yyin == 0)
+    {
+        fprintf(stderr, "Cannot open file %s\n", argv[1]);
+        exit(2);
+    }
 
-  hashPrint();
-  // printf("Main done! File has %d lines\n", lineNumber);
-  printf(stderr, "Compilation succesfull!\n");
-  exit(0);
+    output = fopen(argv[2], "w+");
+    if (output == 0)
+    {
+        fprintf(stderr, "Cannot open file %s\n", argv[2]);
+        exit(2);
+    }
+
+    hashInit();
+
+    fprintf(stderr, "Starting parser...\n");
+    yyparse();
+    fprintf(stderr, "Parser finished.\n");
+
+    AST *root = getRoot();
+    if (root == 0)
+    {
+        fprintf(stderr, "Root is null.\n");
+        exit(3);
+    }
+
+    fprintf(stderr, "Printing AST code to output file...\n");
+    astPrintCode(root, output);
+    fprintf(stderr, "AST code printed.\n");
+
+    fclose(output);
+
+    fprintf(stderr, "Compilation successful!\n");
+    return 0;
 }
