0b3b9f73819af2769b1f2a6817141a7b60695ddb
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2025-01-04 20:51:34 -0300

    feat MAP CONTAINERRRR

--- 22_2/part 4/mapContainer.cpp
+++ 22_2/part 4/mapContainer.cpp
@@ -8,9 +8,38 @@ map<string, Symbol> symbolTable;
 
 Symbol *insertSymbol(const string &text, int type)
 {
-  auto result = symbolTable.emplace(text, Symbol{type, text});
+  auto it = symbolTable.find(text);
+  if (it != symbolTable.end())
+  {
+    return &(it->second);
+  }
 
-  return &(result.first->second);
+  symbolTable[text] = Symbol{type, -1, text};
+  Symbol *symbol = &symbolTable[text];
+
+  switch (type)
+  {
+  case SYMBOL_LIT_INT:
+    symbol->dataType = DATATYPE_INT;
+    break;
+  case SYMBOL_LIT_CHAR:
+    symbol->dataType = DATATYPE_CHAR;
+    break;
+  // case SYMBOL_LIT_TRUE: // duno if scanner should have true and false
+  // case SYMBOL_LIT_FALSE:
+  //   symbol->dataType = DATATYPE_BOOL;
+  //   break;
+  case SYMBOL_IDENTIFIER:
+    symbol->dataType = -1;
+    break;
+  default:
+    if (type != SYMBOL_LIT_STRING)
+      cerr << "Warning: Unknown symbol type for text '" << text << "'." << endl;
+    symbol->dataType = -1;
+    break;
+  }
+
+  return symbol;
 }
 
 void printSymbolTable()
--- 22_2/part 4/mapContainer.h
+++ 22_2/part 4/mapContainer.h
@@ -5,10 +5,27 @@
 #include <string>
 #include <map>
 
+#define SYMBOL_LIT_INT 1
+#define SYMBOL_LIT_CHAR 2
+#define SYMBOL_LIT_STRING 3
+#define SYMBOL_LIT_TRUE 4
+#define SYMBOL_LIT_FALSE 5
+#define SYMBOL_IDENTIFIER 6
+
+#define SYMBOL_VAR 10
+#define SYMBOL_VECTOR 11
+#define SYMBOL_FUNC 12
+#define SYMBOL_PARAM 13
+
+#define DATATYPE_CHAR 20
+#define DATATYPE_INT 21
+#define DATATYPE_BOOL 22
+
 using namespace std;
 struct Symbol
 {
   int type;
+  int dataType;
   string text;
 };
 
